6d0339a7f4c236e102274c401fd9e164336be6b4
Kartekay Kaushik <128483491+kartekay@users.noreply.github.com>
2023-03-22 10:43:49 +0530

    Add files via upload

---
+++ code2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main (){
+    int fav_num;
+    std::cout << "Enter fav number between 0 to 100 : ";
+    std::cin >> fav_num;
+    std::cout << "thats my fav num too :" << fav_num; 
+    return 0;
+}
